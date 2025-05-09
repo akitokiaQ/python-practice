48088af402293df68aba83adf35b306e8937714f
Akito <decision_a@icloud.com>
2025-05-09 23:02:26 +0900

    Day3: for / while 練習

---
+++ day3_loops.py
@@ -0,0 +1,11 @@
+# 2025-05-09 : for / while 練習
+numbers = [1,2,3,4,5,6,7,8,9,10]
+for number in numbers :
+    print(f"{number} を出力しました")
+total = sum(numbers)
+print(f"1〜10 の合計は {total} です")
+
+x = 10
+while x >= 0 :
+    print(x)
+    x -= 1
